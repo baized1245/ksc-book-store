4e37804827b3e5cc84d3ba9947563dbccc2abe9c
unknown <you@example.com>
2024-01-27 13:11:16 +0600

    Add origin

--- book-server/index.js
+++ book-server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(
   cors({
-    origin: '',
+    origin: 'https://mern-book-store-1028f.web.app',
     method: ['POST', 'GET', 'PATCH', 'PUT', 'DELETE'],
     credential: true,
   })
