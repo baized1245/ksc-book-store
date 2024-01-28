a0eafb388efaaa5fa169595bcbe5fdfbb005cfef
unknown <you@example.com>
2024-01-28 20:23:14 +0600

    Customize in blog and about page

--- book-client/src/components/About.jsx
+++ book-client/src/components/About.jsx
@@ -2,33 +2,43 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="text-center p-10 w-1/2 h-screen items-center text-wrap mt-10 justify-center">
-      <h2 className="text-blue-600 font-semibold text-2xl my-3 text-center">
-        KSC Book Store
-      </h2>
-      <p className="font-medium text-base my-2 text-center">
-        Welcome to our book community! Our mission is to provide a platform
-        where book enthusiasts can connect, share, and explore the vast world of
-        literature. Whether you're an avid reader, a passionate writer, or
-        someone simply looking for your next great read, you've come to the
-        right place. At [Your Website Name], we believe in the power of
-        storytelling to inspire, educate, and entertain. With our user-friendly
-        interface, you can easily browse through a diverse collection of books,
-        ranging from timeless classics to contemporary bestsellers. But we're
-        more than just a virtual library. We're a community-driven space where
-        members can upload, edit, and share their own literary creations.
-        Whether you're a budding author or an established writer, we welcome you
-        to showcase your work and engage with fellow book lovers. Our commitment
-        to inclusivity means that everyone is welcome here, regardless of
-        background, genre preference, or reading level. We believe that
-        diversity enriches our community and allows us to explore new
-        perspectives through the pages of a book. So, whether you're here to
-        discover your next favorite novel, connect with like-minded individuals,
-        or unleash your creative potential, we invite you to join us on this
-        literary journey. Together, let's celebrate the magic of storytelling
-        and build a vibrant community around the love of books. Thank you for
-        being a part of [Your Website Name]. Happy reading!
-      </p>
+    <div className="w-screen p-10 h-screen flex items-center mt-10 justify-center">
+      <div className="text-center w-1/2 h-screen text-wrap ">
+        <h2 className="text-blue-600 font-semibold text-2xl my-3 text-center">
+          KSC Book Store
+        </h2>
+        <p className="font-medium text-base my-2 text-center">
+          Welcome to our book community! Our mission is to provide a platform
+          where book enthusiasts can connect, share, and explore the vast world
+          of literature. Whether you're an avid reader, a passionate writer, or
+          someone simply looking for your next great read, you've come to the
+          right place. At [Your Website Name], we believe in the power of
+          storytelling to inspire, educate, and entertain. With our
+          user-friendly interface, you can easily browse through a diverse
+          collection of books, ranging from timeless classics to contemporary
+          bestsellers. But we're more than just a virtual library. We're a
+          community-driven space where members can upload, edit, and share their
+          own literary creations. Whether you're a budding author or an
+          established writer, we welcome you to showcase your work and engage
+          with fellow book lovers. Our commitment to inclusivity means that
+          everyone is welcome here, regardless of background, genre preference,
+          or reading level. We believe that diversity enriches our community and
+          allows us to explore new perspectives through the pages of a book. So,
+          whether you're here to discover your next favorite novel, connect with
+          like-minded individuals, or unleash your creative potential, we invite
+          you to join us on this literary journey. Together, let's celebrate the
+          magic of storytelling and build a vibrant community around the love of
+          books. Thank you for being a part of [Your Website Name]. Happy
+          reading!
+        </p>
+      </div>
+      <div className="w-1/2 h-screen">
+        <img
+          src="https://img.freepik.com/free-vector/online-library-isometric_98292-7026.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=ais"
+          alt=""
+          className="rounded-md w-screen h-screen"
+        />
+      </div>
     </div>
   )
 }
--- book-client/src/components/Blog.jsx
+++ book-client/src/components/Blog.jsx
@@ -2,34 +2,44 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div className="text-center p-10 h-screen w-1/2 items-center text-wrap mt-10 justify-center">
-      <h1 className="text-blue-600 font-semibold text-2xl my-3 text-center">
-        Welcome to our Book Blog!
-      </h1>
-      <p className="font-medium text-base my-2 text-center text-wrap">
-        Here at KSC Book Store, we're not just passionate about reading books;
-        we're also avid writers and thinkers who love to share our thoughts and
-        insights about literature, writing, and everything in between. Our blog
-        is a space where we delve deeper into the world of books, explore
-        literary trends, discuss writing techniques, and share recommendations
-        with our community. Whether you're a seasoned writer looking for
-        inspiration, a bookworm seeking new perspectives, or someone simply
-        curious about the literary landscape, you'll find something to pique
-        your interest on our blog. From book reviews and author interviews to
-        writing tips and discussions on literary themes, our blog covers a wide
-        range of topics to cater to every reader and writer's tastes. But our
-        blog isn't just about us; it's also about you, our valued community
-        members. We encourage you to join the conversation by sharing your own
-        thoughts, insights, and recommendations in the comments section. After
-        all, the joy of reading is best experienced when shared with others, and
-        we believe in fostering a dynamic and interactive community of book
-        lovers. So, whether you're looking for your next great read, seeking
-        inspiration for your next writing project, or simply craving some
-        literary discourse, we invite you to explore our blog and become a part
-        of our vibrant bookish community. Together, let's celebrate the magic of
-        words and embark on a journey of exploration and discovery through the
-        pages of our blog. Happy reading and writing!
-      </p>
+    <div className="w-screen p-10 h-screen flex items-center mt-10 justify-center">
+      <div className="text-center w-1/2 h-screen text-wrap">
+        <h1 className="text-blue-600 font-semibold text-2xl my-3 text-center">
+          Welcome to our Book Blog!
+        </h1>
+        <p className="font-medium text-base my-2 text-center text-wrap">
+          Here at KSC Book Store, we're not just passionate about reading books;
+          we're also avid writers and thinkers who love to share our thoughts
+          and insights about literature, writing, and everything in between. Our
+          blog is a space where we delve deeper into the world of books, explore
+          literary trends, discuss writing techniques, and share recommendations
+          with our community. Whether you're a seasoned writer looking for
+          inspiration, a bookworm seeking new perspectives, or someone simply
+          curious about the literary landscape, you'll find something to pique
+          your interest on our blog. From book reviews and author interviews to
+          writing tips and discussions on literary themes, our blog covers a
+          wide range of topics to cater to every reader and writer's tastes. But
+          our blog isn't just about us; it's also about you, our valued
+          community members. We encourage you to join the conversation by
+          sharing your own thoughts, insights, and recommendations in the
+          comments section. After all, the joy of reading is best experienced
+          when shared with others, and we believe in fostering a dynamic and
+          interactive community of book lovers. So, whether you're looking for
+          your next great read, seeking inspiration for your next writing
+          project, or simply craving some literary discourse, we invite you to
+          explore our blog and become a part of our vibrant bookish community.
+          Together, let's celebrate the magic of words and embark on a journey
+          of exploration and discovery through the pages of our blog. Happy
+          reading and writing!
+        </p>
+      </div>
+      <div className="w-1/2 h-screen">
+        <img
+          src="https://img.freepik.com/free-vector/book-shop-isometric-illustration_1284-19711.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=ais"
+          alt=""
+          className="rounded-md w-screen h-screen"
+        />
+      </div>
     </div>
   )
 }
